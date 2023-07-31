b99761a40bd122c593ff9cdb687715b901939049
gitae Kim <rlarlxo2323@naver.com>
2023-08-01 00:14:48 +0900

    date: 2023-08-01

--- src/boj/Q2178.java
+++ src/boj/Q2178.java
@@ -12,11 +12,54 @@ public class Q2178 {
     public int[][] arr;
     public int[] dx = {-1, 0, 1, 0};
     public int[] dy = {0, 1, 0, -1};
+    public int N,M;
+
+    public Pos getPos(int idx, Pos now) {
+        int x = now.x;
+        int y = now.y;
+        int level = now.level;
+        int ix = dx[idx];
+        int iy = dy[idx];
+
+        if (x+ix >= 0 && x+ix < arr.length
+        && y+iy >= 0 && y+iy < arr[0].length
+        && arr[x+ix][y+iy] != 0) {
+            x += ix;
+            y += iy;
+        }
+        return new Pos(x, y, level + 1);
+    }
+
+    public int bfs(Pos start) {
+        Queue<Pos> q = new LinkedList<>();
+        visited[start.x][start.y] = true;
+        q.add(start);
+        while (!q.isEmpty()) {
+            Pos now = q.poll();
+            int x = now.x;
+            int y = now.y;
+            int level = now.level;
+
+            if (x == N-1 && y == M-1) {
+                return level;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Pos next = getPos(i, now);
+                if (!visited[next.x][next.y]) {
+                    visited[next.x][next.y] = true;
+                    q.add(next);
+                }
+            }
+        }
+        return -1;
+    }
+
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
         visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
@@ -25,12 +68,7 @@ public class Q2178 {
                 arr[i][j] = Character.getNumericValue(str.charAt(j));
             }
         }
-        bfs(0, 0);
-    }
-
-    public void bfs(int ix, int iy) {
-        Queue<Integer> q = new LinkedList<>();
-//        visited
+        System.out.println(bfs(new Pos(0, 0, 1)));
     }
 
     public static void main(String[] args) throws IOException {
@@ -41,5 +79,11 @@ public class Q2178 {
 class Pos {
     public int x;
     public int y;
+    public int level;
 
+    public Pos(int x, int y, int level) {
+        this.x = x;
+        this.y = y;
+        this.level = level;
+    }
 }
